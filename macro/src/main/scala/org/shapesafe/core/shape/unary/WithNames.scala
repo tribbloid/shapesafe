25c99326750a556a422e77c692b4a6052b677b66
tribbloid <pc175@uow.edu.au>
2021-02-20 14:36:44 -0500

    DEAD LOOP!

--- macro/src/main/scala/org/shapesafe/core/shape/unary/WithNames.scala
+++ macro/src/main/scala/org/shapesafe/core/shape/unary/WithNames.scala
@@ -15,21 +15,43 @@ case class WithNames[
 
 object WithNames {
 
-  implicit def asLeaf[
-      S1 <: Shape,
+//  implicit def asLeaf[
+//      S1 <: Shape,
+//      P1 <: LeafShape,
+//      N <: Names,
+//      HO <: HList,
+//      O <: LeafShape
+//  ](
+//      implicit
+//      lemma: S1 ~~> P1,
+//      zip: ZipWithKeys.Aux[N#Keys, P1#_Dimensions#Static, HO],
+//      toShape: LeafShape.FromRecord.==>[HO, O]
+//  ): WithNames[S1, N] =>> O = {
+//    from[WithNames[S1, N]].=>> { src =>
+//      val keys: N#Keys = src.newNames.keys
+//      val p1: P1 = lemma.valueOf(src.s1)
+//
+//      val values: P1#_Dimensions#Static = p1.dimensions.static
+//
+//      val zipped: HO = values.zipWithKeys(keys)
+//      LeafShape.FromRecord(zipped)
+//    }
+//  }
+
+  // TODO: DEAD LOOP!
+  implicit def axiom[
       P1 <: LeafShape,
       N <: Names,
       HO <: HList,
       O <: LeafShape
   ](
       implicit
-      lemma: S1 ~~> P1,
       zip: ZipWithKeys.Aux[N#Keys, P1#_Dimensions#Static, HO],
       toShape: LeafShape.FromRecord.==>[HO, O]
-  ): WithNames[S1, N] =>> O = {
-    from[WithNames[S1, N]].=>> { src =>
+  ): WithNames[P1, N] =>> O = {
+    from[WithNames[P1, N]].=>> { src =>
       val keys: N#Keys = src.newNames.keys
-      val p1: P1 = lemma.valueOf(src.s1)
+      val p1: P1 = src.s1
 
       val values: P1#_Dimensions#Static = p1.dimensions.static
 
@@ -38,44 +60,22 @@ object WithNames {
     }
   }
 
-  // TODO: DEAD LOOP!
-//  implicit def axiom[
-//      P1 <: LeafShape,
-//      N <: Names,
-//      HO <: HList,
-//      O <: LeafShape
-//  ](
-//      implicit
-//      zip: ZipWithKeys.Aux[N#Keys, P1#_Dimensions#Static, HO],
-//      toShape: LeafShape.FromRecord.==>[HO, O]
-//  ): WithNames[P1, N] =>> O = {
-//    from[WithNames[P1, N]].=>> { src =>
-//      val keys: N#Keys = src.newNames.keys
-//      val p1: P1 = src.s1
-//
-//      val values: P1#_Dimensions#Static = p1.dimensions.static
-//
-//      val zipped: HO = values.zipWithKeys(keys)
-//      LeafShape.FromRecord(zipped)
-//    }
-//  }
-//
-//  implicit def theorem[
-//      S1 <: Shape,
-//      N <: Names,
-//      P1 <: LeafShape,
-//      O <: LeafShape
-//  ](
-//      implicit
-//      lemma1: S1 ~~> P1,
-//      lemma2: WithNames[P1, N] --> O
-//  ): WithNames[S1, N] =>> O = {
-//    from[WithNames[S1, N]].=>> { src =>
-//      val p1: P1 = lemma1.valueOf(src.s1)
-//
-//      lemma2.valueOf(
-//        src.copy(p1)
-//      )
-//    }
-//  }
+  implicit def theorem[
+      S1 <: Shape,
+      N <: Names,
+      P1 <: LeafShape,
+      O <: LeafShape
+  ](
+      implicit
+      lemma1: S1 ~~> P1,
+      lemma2: WithNames[P1, N] --> O
+  ): WithNames[S1, N] =>> O = {
+    from[WithNames[S1, N]].=>> { src =>
+      val p1: P1 = lemma1.valueOf(src.s1)
+
+      lemma2.valueOf(
+        src.copy(p1)
+      )
+    }
+  }
 }
