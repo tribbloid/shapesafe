ae19100f0cf212e936db957ed9d69b37d6a88c62
peng <pc175@uow.edu.au>
2021-04-22 15:39:54 -0400

    improve build scripts and CI

--- dev/make-all.sh
+++ dev/make-all.sh
@@ -7,8 +7,8 @@ FWDIR="$(
 DATE=$(date --iso-8601=second)
 
 echo "[SUBMODULE(S)]" && \
-cd splain && \
-sbt publishM2 && \
+cd ${FWDIR}/splain && \
+sbt clean publishM2 && \
 cd ..
 
 mkdir -p ${FWDIR}/logs
@@ -17,6 +17,11 @@ mkdir -p ${FWDIR}/logs/dependencyTree
 ${FWDIR}/gradlew -q dependencyTree "${@}" > ${FWDIR}/logs/dependencyTree/"$DATE".log
 
 ${FWDIR}/gradlew clean testClasses publishToMavenLocal "${@}"
+
+echo "[DEMO]" && \
+cd ${FWDIR}/shapesafe-demo && \
+sbt clean compile && \
+cd ..
 
 #CD ${FWDIR}/shapeshape-demo/
 #
