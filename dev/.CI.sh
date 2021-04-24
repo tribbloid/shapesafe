301bc25e3fb8a039fa98d34b67e0f3c0656fde93
peng <pc175@uow.edu.au>
2021-04-24 16:03:30 -0400

    submodule update failure will cause the CI build to fail

--- dev/.CI.sh
+++ dev/.CI.sh
@@ -5,10 +5,7 @@ CRDIR="$(
   pwd
 )"
 
-git submodule sync # this is required if any git module has been initialised to another remote repo
-git submodule foreach git fetch
-git submodule update --init --recursive
-
+"${CRDIR}"/.update-submodules.sh && \
 echo "[COMPILING]" && \
 "${CRDIR}"/make-all.sh "${@}" && \
 echo "[RUNNING TESTS]" && \
