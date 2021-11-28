3808ac1525060544284b8af23f0042c8c7ae0455
Peng Cheng <pc175@uow.edu.au>
2021-11-28 14:05:21 -0500

    upgrade dependencies
    part of gradle idea settings are applied on all projects
    
    minor cleanup

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,10 +2,6 @@ val vs: Versions = versions()
 
 dependencies {
 
-    testFixturesApi(testFixtures(project(":graph-commons-core")))
-
     api(project(":macro"))
-
-//    api("eu.timepit:refined_${vv.scalaBinaryV}:0.9.14")
-//    /TODO: remove, most arity inspection macros doesn't work on collection/tuple, using shapeless Length as cheap alternative
+    testFixturesApi(testFixtures(project(":macro")))
 }
--- macro/build.gradle.kts
+++ macro/build.gradle.kts
@@ -3,9 +3,5 @@ val vs: Versions = versions()
 dependencies {
 
     api(project(":graph-commons-core"))
-
     testFixturesApi(testFixtures(project(":graph-commons-core")))
-
-//    api("eu.timepit:refined_${vv.scalaBinaryV}:0.9.14")
-//    /TODO: remove, most arity inspection macros doesn't work on collection/tuple, using shapeless Length as cheap alternative
 }
