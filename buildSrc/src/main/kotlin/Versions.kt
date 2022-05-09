1e9238786b666db45a0aeecaee09b0d7072b7d77
Peng Cheng <pc175@uow.edu.au>
2022-05-09 16:14:48 -0400

    upgrade scalatestV (again!)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -23,7 +23,7 @@ class Versions(self: Project) {
 
     val sparkV: String = self.properties.get("sparkVersion").toString()
 
-    val scalaTestV: String = "3.2.11"
+    val scalaTestV: String = "3.2.12"
 
     val splainV: String = self.properties.get("splainVersion")?.toString() ?: ""
 }
