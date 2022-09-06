ed93a6efa6d654015d874c9be7bcb1136f26550c
peng <pc175@uow.edu.au>
2022-09-06 14:25:36 -0400

    upgrade submodule & a dependency

--- verify/breeze/build.gradle.kts
+++ verify/breeze/build.gradle.kts
@@ -6,7 +6,7 @@ dependencies {
     testImplementation(testFixtures(project(":verify")))
 
     // https://mvnrepository.com/artifact/org.scalanlp/breeze
-    api("org.scalanlp:breeze_${vs.scalaBinaryV}:2.0")
+    api("org.scalanlp:breeze_${vs.scalaBinaryV}:2.1.0")
     // https://mvnrepository.com/artifact/org.freehep/jas-core
     // TODO: add this library to allow visualization of expressions
 //    api("org.freehep:jas-core:3.2.0")
