25976776a5a38537cc7f209413bdce1474cd0c53
Peng Cheng <peng.cheng@acyclic.ai>
2023-05-28 16:18:34 -0400

    upgrade submodule

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
 
     signing
     `maven-publish`
-    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 
     id("com.github.ben-manes.versions") version "0.44.0"
 }
