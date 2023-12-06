44511134c0e42c065c7adc68dcfa5cfa9d4a6348
peng <peng.cheng@acyclic.ai>
2023-12-06 16:06:04 -0500

    improve submodule, opt out from recursive
    buildSrc is now a submodule
    
    upgrade several dependencies

--- verify/djl/build.gradle.kts
+++ verify/djl/build.gradle.kts
@@ -6,6 +6,6 @@ dependencies {
     testImplementation(testFixtures(project(":verify")))
 
     // TODO: the following are too specialised and should only be used in testing
-    api("ai.djl.pytorch:pytorch-engine:0.22.1")
+    api("ai.djl.pytorch:pytorch-engine:0.25.0")
     api("ai.djl.pytorch:pytorch-native-auto:1.9.1")
 }
